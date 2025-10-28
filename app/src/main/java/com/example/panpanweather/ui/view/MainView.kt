f6fd0f36a2308be22ce21866b15bf267f56b797a
nikigitdev <nikigxd2006@gmail.com>
2025-10-28 18:45:05 +0700

    start

---
+++ app/src/main/java/com/example/panpanweather/ui/view/MainView.kt
@@ -0,0 +1,18 @@
+package com.example.panpanweather.ui.view
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun MainView (
+    modifier: Modifier = Modifier
+) {
+
+}
+
+@Composable
+@Preview(showBackground = true, showSystemUi = true)
+fun MainViewPreview() {
+    MainView()
+}
