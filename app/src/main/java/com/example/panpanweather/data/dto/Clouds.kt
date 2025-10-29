16d28a9a1eee3816e2fe3a1be2fed7b2aafb35a9
nikigitdev <nikigxd2006@gmail.com>
2025-10-29 12:55:56 +0700

    DTO

---
+++ app/src/main/java/com/example/panpanweather/data/dto/Clouds.kt
@@ -0,0 +1,5 @@
+package com.example.panpanweather.data.dto
+
+data class Clouds(
+    val all: Int
+)
---
+++ app/src/main/java/com/example/panpanweather/data/dto/Coord.kt
@@ -0,0 +1,6 @@
+package com.example.panpanweather.data.dto
+
+data class Coord(
+    val lat: Double,
+    val lon: Double
+)
